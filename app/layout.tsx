1c5101e809954c57383319f7954e20668a23ade1
wooleejaan <wooleejaan@gmail.com>
2023-09-02 17:19:38 +0900

    chore: injecting meta data into a proj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-import type { Metadata } from 'next'
-
 import '@/libs/shared/styles/global.scss'
 
-export const metadata: Metadata = {
-  title: 'August Archive',
-  description: '배우고 느낀 것들을 모아두는 공간',
-}
+import { META_ROOT } from './_meta'
+
+export const metadata = META_ROOT
 
 export default function RootLayout({
   children,
