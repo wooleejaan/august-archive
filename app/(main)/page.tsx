49d067dd970b18127d6e781003875df877b76465
wooleejaan <wooleejaan@gmail.com>
2023-09-02 23:14:30 +0900

    feat: adding github api annotations to a page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,10 @@
+// import 'highlight.js/styles/github-dark.css'
+
 // import { getPosts } from '@/libs/shared/hooks/getPosts.hook'
 
 export default async function HomePage() {
   // const data = await getPosts('contents/projects/')
 
   // return <div>{data[0].content}</div>
-  return <div>page</div>
+  return <div>homepage</div>
 }
