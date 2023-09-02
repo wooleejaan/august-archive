05330ae52c75b89ded449c91899fdc5b5317934f
wooleejaan <wooleejaan@gmail.com>
2023-09-02 16:29:10 +0900

    chore: adding global.scss to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,20 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
+
+import '@/libs/shared/styles/global.scss'
 
 export const metadata: Metadata = {
-  title: "August Archive",
-  description: "배우고 느낀 것들을 모아두는 공간",
-};
+  title: 'August Archive',
+  description: '배우고 느낀 것들을 모아두는 공간',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="ko">
       <body>{children}</body>
     </html>
-  );
+  )
 }
