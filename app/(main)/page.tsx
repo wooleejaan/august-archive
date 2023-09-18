63a4594e88030e4b6aff6aa113e3f7fcbdf84735
wooleejaan <wooleejaan@gmail.com>
2023-09-18 21:40:38 +0900

    fix: partialPageResponse type error

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,18 @@
 // import 'highlight.js/styles/github-dark.css'
-import { PartialPageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
-
 import { getPages } from '@/libs/shared/helpers/notion.helpers'
+
+interface PartialPageObjectResponseMore {
+  object: 'page'
+  id: string
+  properties?: {
+    Slug: {
+      rich_text: Array<{ plain_text: string }>
+    }
+    Title: {
+      title: Array<{ plain_text: string }>
+    }
+  }
+}
 
 export default async function HomePage() {
   const [archives, projects] = await Promise.all([
@@ -13,14 +24,14 @@ export default async function HomePage() {
   const archiveList: Array<{ slug: string; title: string }> = []
 
   // eslint-disable-next-line no-restricted-syntax
-  for (const page of projects.results as PartialPageObjectResponse[]) {
+  for (const page of projects.results as PartialPageObjectResponseMore[]) {
     projectList.push({
       slug: page.properties?.Slug.rich_text[0].plain_text as string,
       title: page.properties?.Title.title[0].plain_text as string,
     })
   }
   // eslint-disable-next-line no-restricted-syntax
-  for (const archive of archives.results as PartialPageObjectResponse[]) {
+  for (const archive of archives.results as PartialPageObjectResponseMore[]) {
     archiveList.push({
       slug: archive.properties?.Slug.rich_text[0].plain_text as string,
       title: archive.properties?.Title.title[0].plain_text as string,
