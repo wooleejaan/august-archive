f3690a1aae3cf6d887e861ae60d9b7df12ee97a2
wooleejaan <wooleejaan@gmail.com>
2023-09-19 19:27:13 +0900

    feat: add footer

---
+++ libs/footer/ui/footer.ui.tsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames/bind'
+
+import styles from './footer.module.scss'
+
+const cx = classNames.bind(styles)
+
+export default function UiFooter() {
+  return (
+    <div className={cx('footer')}>&copy; 2023 yongwoo all rights reserved.</div>
+  )
+}
