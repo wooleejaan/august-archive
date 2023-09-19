b533f6ee7ad2b11cc8f577bd3db04cd1c72de351
wooleejaan <wooleejaan@gmail.com>
2023-09-19 16:31:17 +0900

    feat: add UiDots to Section

--- libs/heroMain/feature/heroMainArchives.feature.tsx
+++ libs/heroMain/feature/heroMainArchives.feature.tsx
@@ -1,6 +1,13 @@
+import UiDots from '@/libs/dots/ui/dots.ui'
+
 import { HeroMainSectionProps } from '../shared/types/heroMain.type'
 import UiHeroMainSection from '../ui/heroMainSection.ui.tsx'
 
 export default function HeroMainArchives({ section }: HeroMainSectionProps) {
-  return <UiHeroMainSection section={section} sectionType="archives" />
+  return (
+    <>
+      <UiHeroMainSection section={section} sectionType="archives" />
+      <UiDots sectionType="archives" />
+    </>
+  )
 }
--- libs/heroMain/feature/heroMainProjects.feature.tsx
+++ libs/heroMain/feature/heroMainProjects.feature.tsx
@@ -1,6 +1,13 @@
+import UiDots from '@/libs/dots/ui/dots.ui'
+
 import { HeroMainSectionProps } from '../shared/types/heroMain.type'
 import UiHeroMainSection from '../ui/heroMainSection.ui.tsx'
 
 export default function HeroMainProjects({ section }: HeroMainSectionProps) {
-  return <UiHeroMainSection section={section} sectionType="projects" />
+  return (
+    <>
+      <UiHeroMainSection section={section} sectionType="projects" />
+      <UiDots sectionType="projects" />
+    </>
+  )
 }
