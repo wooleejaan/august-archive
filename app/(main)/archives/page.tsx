67c9abaa3a6ce37508e45f4e97f14e2c1e7b5ae3
wooleejaan <wooleejaan@gmail.com>
2023-09-19 23:31:13 +0900

    feat: compound list page

--- app/(main)/archives/page.tsx
+++ app/(main)/archives/page.tsx
@@ -4,6 +4,9 @@ import { notFound } from 'next/navigation'
 import { getPages } from '@/libs/shared/helpers/notion.helpers'
 import { PartialPageObjectResponseMore } from '@/libs/shared/types/page.type'
 
+import UiListMain from '@/libs/listMain/ui/listMain'
+import ListMainSection from '@/libs/listMain/ui/listMainSection'
+
 export default async function ArchivesPage({
   searchParams,
 }: {
@@ -12,18 +15,27 @@ export default async function ArchivesPage({
   const archives = await getPages('archive', 10, searchParams.cursor)
   if (!archives) notFound()
 
-  const nextCursor = archives.next_cursor
-  const archiveList: Array<{ slug: string; title: string }> = []
+  // const nextCursor = archives.next_cursor
+  const archiveList: Array<{
+    slug: string
+    title: string
+    subTitle: string
+    createdDate: string
+  }> = []
 
   // eslint-disable-next-line no-restricted-syntax
   for (const archive of archives.results as PartialPageObjectResponseMore[]) {
     archiveList.push({
       slug: archive.properties?.Slug.rich_text[0].plain_text as string,
       title: archive.properties?.Title.title[0].plain_text as string,
+      subTitle: archive.properties?.SubTitle.rich_text[0].plain_text as string,
+      createdDate: new Date(archive.created_time).toLocaleDateString(),
     })
   }
 
-  console.log(archiveList, nextCursor)
-
-  return <div>archive page</div>
+  return (
+    <UiListMain listTitle="archives">
+      <ListMainSection section={archiveList} sectionType="archives" />
+    </UiListMain>
+  )
 }
--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -4,6 +4,9 @@ import { notFound } from 'next/navigation'
 import { getPages } from '@/libs/shared/helpers/notion.helpers'
 import { PartialPageObjectResponseMore } from '@/libs/shared/types/page.type'
 
+import UiListMain from '@/libs/listMain/ui/listMain'
+import ListMainSection from '@/libs/listMain/ui/listMainSection'
+
 export default async function ProjectsPage({
   searchParams,
 }: {
@@ -12,18 +15,27 @@ export default async function ProjectsPage({
   const projects = await getPages('project', 10, searchParams.cursor)
   if (!projects) notFound()
 
-  const nextCursor = projects.next_cursor
-  const projectList: Array<{ slug: string; title: string }> = []
+  // const nextCursor = projects.next_cursor
+  const projectList: Array<{
+    slug: string
+    title: string
+    subTitle: string
+    createdDate: string
+  }> = []
 
   // eslint-disable-next-line no-restricted-syntax
   for (const page of projects.results as PartialPageObjectResponseMore[]) {
     projectList.push({
       slug: page.properties?.Slug.rich_text[0].plain_text as string,
       title: page.properties?.Title.title[0].plain_text as string,
+      subTitle: page.properties?.SubTitle.rich_text[0].plain_text as string,
+      createdDate: new Date(page.created_time).toLocaleDateString(),
     })
   }
 
-  console.log(projectList, nextCursor)
-
-  return <div>project page</div>
+  return (
+    <UiListMain listTitle="projects">
+      <ListMainSection section={projectList} sectionType="projects" />
+    </UiListMain>
+  )
 }
