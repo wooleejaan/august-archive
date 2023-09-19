75cc7f6d1d9f7200be665d4bbfbed0812a51e764
Yong-Woo, Lee <110705019+wooleejaan@users.noreply.github.com>
2023-09-19 16:37:21 +0900

    feature add show-more button (#47)
    * feat: implement smooth scroll
    
    * fix: heading3 font-size
    
    * feat: UiDots Component
    
    * feat: add UiDots to Section
    
    * fix: mainpage layout & hover animation

---
+++ libs/dots/ui/dots.ui.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind'
+
+import Link from 'next/link'
+
+import styles from './dots.module.scss'
+
+const cx = classNames.bind(styles)
+
+interface UiDotsProps {
+  sectionType: 'archives' | 'projects'
+}
+
+export default function UiDots({ sectionType }: UiDotsProps) {
+  return (
+    <Link
+      href={`/${sectionType}`}
+      shallow={true}
+      replace={true}
+      className={cx('dots')}
+    >
+      <div className={cx('dot')}></div>
+      <div className={cx('dot')}></div>
+      <div className={cx('dot')}></div>
+    </Link>
+  )
+}
