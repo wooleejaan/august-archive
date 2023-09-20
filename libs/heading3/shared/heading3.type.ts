69a1a9d91e9a59ab991f27b32a8a1ca8d5df0a1d
wooleejaan <wooleejaan@gmail.com>
2023-09-20 20:09:29 +0900

    refactor: add color in Heading3 prop

--- libs/heading3/shared/heading3.type.ts
+++ libs/heading3/shared/heading3.type.ts
@@ -2,6 +2,7 @@ import { HTMLAttributes } from 'react'
 
 interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
   children?: React.ReactNode
+  color?: string
 }
 
 export type { HeadingProps }
--- libs/heading3/ui/heading3.ui.tsx
+++ libs/heading3/ui/heading3.ui.tsx
@@ -7,9 +7,9 @@ import styles from './heading3.module.scss'
 
 const cx = classNames.bind(styles)
 
-export default function Heading3({ children, ...props }: HeadingProps) {
+export default function Heading3({ children, color, ...props }: HeadingProps) {
   return (
-    <h3 className={cx('h3', 'ch')} {...props}>
+    <h3 className={cx('h3', 'ch')} style={{ color }} {...props}>
       <Link href={`#${props.id}`}>{children}</Link>
     </h3>
   )
