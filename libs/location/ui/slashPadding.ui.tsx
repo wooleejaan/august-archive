a2ff3851ff64da005a1d470c4e5ee6ce3b73dd28
wooleejaan <wooleejaan@gmail.com>
2023-09-20 20:07:33 +0900

    refactor: fix slashPadding

--- libs/location/ui/slashPadding.ui.tsx
+++ libs/location/ui/slashPadding.ui.tsx
@@ -2,6 +2,9 @@ export default function UiSlashPadding() {
   return (
     <span
       style={{
+        color: '#a1a1a1',
+        fontSize: '11px',
+        fontWeight: '500',
         padding: '0 6px',
       }}
     >
