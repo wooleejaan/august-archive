8ad90e6bd42948840c918275c383d31d00137ce1
Yong-Woo, Lee <110705019+wooleejaan@users.noreply.github.com>
2023-09-23 18:32:57 +0900

    fix: SSG => ISR (#67)

--- libs/shared/helpers/getNotion.helper.ts
+++ libs/shared/helpers/getNotion.helper.ts
@@ -1,55 +1,85 @@
-const getPagesHelper = async <T>(
-  containProperty: string,
-  pageSize: number,
-  startCursor?: string,
-): Promise<T> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/notion/list?property=${containProperty}&pageSize=${pageSize}&startCursor=${startCursor}`,
-    {
-      // cache: 'no-store',
-      cache: 'force-cache',
-      // next: { revalidate: 86400 },
-    },
-  )
+import { cache } from 'react'
 
-  if (!res.ok) {
-    // This will activate the closest `error.ts` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+/**
+ *
+ * @description ISR 적용된 API Handler
+ * @example
+ * ```bash
+ * # how to revalidate
+ * GET `/api/revalidate?path=/&secret=${process.env.MY_SECRET_TOKEN}`
+ * ```
+ */
+const getPagesHelper = cache(
+  async <T>(
+    containProperty: string,
+    pageSize: number,
+    startCursor?: string,
+  ): Promise<T> => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/notion/list?property=${containProperty}&pageSize=${pageSize}&startCursor=${startCursor}`,
+      {
+        // cache: 'no-store',
+        // cache: 'force-cache',
+        next: { revalidate: 86400 },
+      },
+    )
 
-  const data = res.json()
-  return data
-}
+    if (!res.ok) {
+      // This will activate the closest `error.ts` Error Boundary
+      throw new Error('Failed to fetch data')
+    }
 
-const getPageBySlugHelper = async <T>(
-  slug: string,
-  containProperty: string,
-): Promise<T> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/notion/listBySlug?property=${containProperty}&slug=${slug}`,
-    {
-      // cache: 'no-store',
-      cache: 'force-cache',
-      // next: { revalidate: 86400 },
-    },
-  )
+    const data = res.json()
+    return data
+  },
+)
 
-  if (!res.ok) {
-    // This will activate the closest `error.ts` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+/**
+ *
+ * @description ISR 적용된 API Handler
+ * @example
+ * ```bash
+ * # how to revalidate
+ * GET `/api/revalidate?path=/&secret=${process.env.MY_SECRET_TOKEN}`
+ * ```
+ */
+const getPageBySlugHelper = cache(
+  async <T>(slug: string, containProperty: string): Promise<T> => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/notion/listBySlug?property=${containProperty}&slug=${slug}`,
+      {
+        // cache: 'no-store',
+        // cache: 'force-cache',
+        next: { revalidate: 86400 },
+      },
+    )
 
-  const data = res.json()
-  return data
-}
+    if (!res.ok) {
+      // This will activate the closest `error.ts` Error Boundary
+      throw new Error('Failed to fetch data')
+    }
 
-const getPageContentHelper = async <T>(pageId: string): Promise<T> => {
+    const data = res.json()
+    return data
+  },
+)
+
+/**
+ *
+ * @description ISR 적용된 API Handler
+ * @example
+ * ```bash
+ * # how to revalidate
+ * GET `/api/revalidate?path=/&secret=${process.env.MY_SECRET_TOKEN}`
+ * ```
+ */
+const getPageContentHelper = cache(async <T>(pageId: string): Promise<T> => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/notion/content?detailId=${pageId}`,
     {
       // cache: 'no-store',
-      cache: 'force-cache',
-      // next: { revalidate: 86400 },
+      // cache: 'force-cache',
+      next: { revalidate: 86400 },
     },
   )
 
@@ -60,6 +90,6 @@ const getPageContentHelper = async <T>(pageId: string): Promise<T> => {
 
   const data = res.json()
   return data
-}
+})
 
 export { getPagesHelper, getPageBySlugHelper, getPageContentHelper }
