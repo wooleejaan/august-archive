59f8c683414a5d02d6bfc1c7095d3f9e08c08e40
wooleejaan <wooleejaan@gmail.com>
2023-09-28 11:42:03 +0900

    fix: gnb isTop (isTop==0 => isTop<=400)

--- libs/gnb/feature/gnb.feature.tsx
+++ libs/gnb/feature/gnb.feature.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useEffect } from 'react'
+
 import classNames from 'classnames/bind'
 
 import { GnbProps } from '@/libs/_shared/types/components.type'
@@ -12,13 +14,17 @@ const cx = classNames.bind(styles)
 export default function Gnb({ title }: GnbProps) {
   const { visible, position } = useGnbView()
 
+  useEffect(() => {
+    console.log(position)
+  }, [position])
+
   return (
     <nav>
       <div className={cx('blurLayer')} aria-hidden="true" />
       <div
         className={cx('wrapper', {
           isView: !visible,
-          isTop: position === 0,
+          isTop: position <= 400,
         })}
       >
         <p className={cx('title')}>{title}</p>
