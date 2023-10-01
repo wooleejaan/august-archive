7e1556fbdade558723ba5621f0a04613de3dc41a
wooleejaan <wooleejaan@gmail.com>
2023-10-01 09:51:55 +0900

    refactor: Applying line-breaks to hero-content text

--- libs/heroMain/ui/heroMain.ui.tsx/index.tsx
+++ libs/heroMain/ui/heroMain.ui.tsx/index.tsx
@@ -22,7 +22,8 @@ export default function UiHeroMain({
         <UiSpacing size={15} />
         <UiParagraph>
           새롭게 알게 된 지식, 직면하고 해결한 기술적인 문제를 글로 정리하고
-          공유합니다. 정적이면서도 부드러운 그래픽과 인터페이스를 사랑합니다.
+          공유합니다. <br />
+          정적이면서도 부드러운 그래픽과 인터페이스를 사랑합니다.
         </UiParagraph>
         <UiParagraph color="#d1d1d1" fontWeight={300} fontSize={13.5}>
           front-end developer
