b802302dbac17a18ee36ad1a14bc994a001dfff3
wooleejaan <wooleejaan@gmail.com>
2023-10-22 16:18:48 +0900

    feat: add gtag

---
+++ libs/_shared/services/ga/gtag.ts
@@ -0,0 +1,22 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ID
+
+export const pageview = (url: URL) => {
+  window.gtag('config', GA_TRACKING_ID as string, {
+    page_path: url,
+  })
+}
+
+interface GTagEvent {
+  action: string
+  category?: string
+  label?: string
+  value?: string
+}
+
+export const event = ({ action, category, label, value }: GTagEvent) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value,
+  })
+}
