c1bbf287391fe0fb7338543e0fc1bc41993ed2d1
wooleejaan <wooleejaan@gmail.com>
2023-10-22 16:17:01 +0900

    fix: change routes elem name (to graphics)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 const URL = 'https://www.augustarchives.kr'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ['', '/tags', '/algorithms', '/archives', '/performances'].map(
+  const routes = ['', '/tags', '/algorithms', '/archives', '/graphics'].map(
     (route) => ({
       url: `${URL}${route}`,
       lastModified: new Date().toISOString(),
