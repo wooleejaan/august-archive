48e3c49fa68f3a884023cba821763fff226cc182
wooleejaan <wooleejaan@gmail.com>
2023-10-22 16:38:52 +0900

    fix: add tags detail sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,12 +3,19 @@ import { MetadataRoute } from 'next'
 const URL = 'https://www.augustarchives.kr'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ['', '/tags', '/algorithms', '/archives', '/graphics'].map(
-    (route) => ({
-      url: `${URL}${route}`,
-      lastModified: new Date().toISOString(),
-    }),
-  )
+  const routes = [
+    '',
+    '/tags/graphics',
+    '/tags/frontend',
+    '/tags/computer-science',
+    '/tags/performance',
+    '/algorithms',
+    '/archives',
+    '/graphics',
+  ].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }))
 
   return [...routes]
 }
