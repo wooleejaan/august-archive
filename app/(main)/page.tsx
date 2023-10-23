892cb39f48cc165548074db4423dc93fa5f02116
yongwoo <wooleejaan@gmail.com>
2023-10-23 21:45:46 +0900

    fix: remove tags, algorithms page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,40 +1,12 @@
-import { notFound } from 'next/navigation'
-
-import {
-  getPagesHelper,
-  getTagListHelper,
-} from '@/libs/_shared/apis/getNotion.api'
-import convertToList from '@/libs/_shared/hooks/convertToList.hook'
-import {
-  PagesHelperResponse,
-  TagListHelperResponse,
-} from '@/libs/_shared/types/responses.type'
-import { HomePageProps } from '@/libs/_shared/types/routers.type'
+import UiDots from '@/libs/dots/ui/dots.ui'
 import HeroMain from '@/libs/heroMain/feature/heroMain.feature'
-import HeroMainArchives from '@/libs/heroMain/feature/heroMainArchives.feature'
 import CurrentLocation from '@/libs/location/feature/currentLocation.feature'
 
-export default async function HomePage({ searchParams }: HomePageProps) {
-  const [archives, algorithms, tags] = await Promise.all([
-    getPagesHelper<PagesHelperResponse>('archive', 5, searchParams.cursor),
-    getPagesHelper<PagesHelperResponse>('algorithm', 5, searchParams.cursor),
-    getTagListHelper<TagListHelperResponse>(),
-  ])
-
-  if (!archives || !algorithms || !tags) notFound()
-
-  const tagList = tags.results[0].properties.Category.multi_select.map(
-    (tag) => tag.name,
-  )
-
-  const archiveList = convertToList(archives)
-  const algorithmList = convertToList(algorithms)
-
+export default async function HomePage() {
   return (
     <main>
-      <HeroMain location={<CurrentLocation />} tags={tagList}>
-        <HeroMainArchives section={archiveList} sectionName="archives" />
-        <HeroMainArchives section={algorithmList} sectionName="algorithms" />
+      <HeroMain location={<CurrentLocation />}>
+        <UiDots sectionType="archives" />
       </HeroMain>
     </main>
   )
