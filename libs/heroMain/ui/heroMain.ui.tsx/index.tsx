0d327683e892c37601f89c6ac2be41d7227accc5
yongwoo <wooleejaan@gmail.com>
2023-11-05 11:36:26 +0900

    chore: Edit the self-intro-text within the paragraph

--- libs/heroMain/ui/heroMain.ui.tsx/index.tsx
+++ libs/heroMain/ui/heroMain.ui.tsx/index.tsx
@@ -18,19 +18,18 @@ export default function UiHeroMain({ children, location }: HeroMainProps) {
         {location}
         <UiSpacing size={15} />
         <UiParagraph>
-          정적이면서도 부드러운 인터페이스와 정제되고 작은 그래픽을 좋아합니다.
-          <br />
+          정적이면서도 부드러운 인터페이스와 정제된 그래픽을 선호합니다.
           <br />
           복잡함보다는 단순함을, 장황한 것보다는 함축적이고 추상적인 것을
           선호합니다.
           <br />
-          일상적인 사물을 낯선 시선으로 재정의하는 걸 좋아합니다.
+          일상적인 사물을 낯선 시선으로 재정의하는 행위를 동경합니다.
           <br />
           복잡하게 얽힌 문제를 쉽게 풀어낼 때와 불필요한 것들을 덜어낼 때, 그
-          과정에서 오는 뿌듯함을 즐깁니다.
+          과정에서 오는 뿌듯함을 느낍니다.
         </UiParagraph>
         <UiParagraph>
-          기술적으로 학습하고 있는 것들은{' '}
+          기술적으로 학습하고 있는 건 주로{' '}
           <Link
             className={cx('underLink')}
             href="https://github.com/wooleejaan/yw-playgrounds"
@@ -38,7 +37,7 @@ export default function UiHeroMain({ children, location }: HeroMainProps) {
           >
             Github
           </Link>
-          에 기록하고 있습니다.
+          에 기록하고 있으며, 이 블로그에서는 지극히 취미의 영역만 다룹니다.
         </UiParagraph>
         <UiContacts />
       </section>
